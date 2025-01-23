99e417b8caf91ca0ca6ac97ca885a1faac051c06
wioletadrobik <wioleta.drobik@gmail.com>
2025-01-23 17:13:18 +0100

    Restrict to one plot, use selectbox

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 # This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 import streamlit as st
 import pandas as pd
 from src.utils.bgg_api import use_bgg_api_directly, use_bgg_api
@@ -12,9 +9,15 @@ def main():
     st.text('This page uses bgg-api by SukiCZ')
     #use_bgg_api()
     bg_data = pd.read_csv("datasets/board_games.csv")
-    st.scatter_chart(bg_data, y='average_rating', x='users_rated',
-                     y_label='Average rating',
-                     x_label='Users rated [N]')
+
+    vars_dict = {
+        'Users rated [N]': 'users_rated',
+        'Mechanics [N]': 'n_mechanics',
+        'Categories [N]': 'n_categories'
+     }
+
+    selection = st.selectbox("Select variable to plot",
+                             vars_dict.keys())
 
     def count_sep_by_commas(row):
         if not pd.isna(row):  # Check if row text is not empty
@@ -22,17 +25,11 @@ def main():
         return 0
 
     bg_data['n_mechanics'] = bg_data["mechanic"].apply(count_sep_by_commas)
-    #st.dataframe(bg_data)
-
-    st.scatter_chart(bg_data, y='average_rating', x='n_mechanics',
-                     y_label='Average rating',
-                     x_label='Mechanics [N]')
-
     bg_data['n_categories'] = bg_data["category"].apply(count_sep_by_commas)
-    st.scatter_chart(bg_data, y='average_rating', x='n_categories',
-                     y_label='Average rating',
-                     x_label='Categories [N]')
 
+    st.scatter_chart(bg_data,
+                     x=vars_dict[selection], x_label=selection,
+                     y='average_rating', y_label='Average rating')
 
 
 if __name__ == '__main__':
